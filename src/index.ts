ee88b049da15df3bace7786ef79c5f41e04b7e65
João Levy <git.qodeless@otlook.com>
2022-02-25 02:34:04 -0300

    Realizado ajuste com a somatória das notas.

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,8 @@ import * as dotenv from "dotenv";
 import express from "express";
 import cors from "cors";
 import helmet from "helmet";
+import { SourceMap } from "module";
+import { totalmem } from "os";
 
 dotenv.config();
 
@@ -50,17 +52,16 @@ app.listen(PORT, () => {
 	
 	}
 
-	let a1 =  new Aluno ('Lendro', 26, 8)
-	let a2 = new Aluno ('Luana', 29, 7)
-	let a3 = new Aluno ('Lais', 30, 10)
-
 	let alunos:Array<Aluno> = [
-		{nome: 'Leandro', idade: 26, nota: 8},
-		{nome: 'Luana', idade: 28, nota: 7},
-		{nome: 'Lais', idade: 30, nota: 10}
+		{nome: 'Leandro', idade: 26, nota: 8.3},
+		{nome: 'Luana', idade: 28, nota: 6.5},
+		{nome: 'Lais', idade: 30, nota: 9.9}
 	];
 
-	let soma = [8 + 7 + 10]
+	let soma = alunos.reduce( ( ant, atual ) => ant + atual.nota, 0 )
 	console.log(soma)
+
+	
+
 	
 });
